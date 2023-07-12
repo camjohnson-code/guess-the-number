794c9646559bbff01d55b6de0ca6e7971b62024b
Cam Johnson <cameron.n.johnson15@gmail.com>
2023-07-12 09:34:15 -0600

    Added game logic

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 "use strict";
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+
+  //when there is no input
+  if (!guess) {
+    displayMessage("⛔️ No number!");
+
+    //when player wins
+  } else if (guess === secretNumber) {
+    displayMessage("🎉 Correct number!");
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+    document.querySelector(".number").textContent = secretNumber;
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
+    }
+
+    //when guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? "📈 Too high!" : "📉 Too low!");
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      displayMessage("💥 You lost the game!");
+      document.querySelector(".score").textContent = 0;
+      document.querySelector("body").style.backgroundColor = "#d30";
+    }
+  }
+});
